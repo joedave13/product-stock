7d285e9d660c63137b8e473c4506564c1aafa92b
Joshua Davian <44218146+joedave13@users.noreply.github.com>
2020-03-01 13:24:39 +0700

    Update Database

--- database/db_stock.sql
+++ database/db_stock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2020 at 05:53 PM
+-- Generation Time: Mar 01, 2020 at 07:23 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -64,8 +64,97 @@ INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `jumlah_small`,
 (1, 'BRG-001', 'Tim-Tam', 30, 30, 30),
 (2, 'BRG-002', 'Oreo Strawberry', 10, 10, 10),
 (3, 'BRG-003', 'Mild', 10, 10, 10),
-(4, 'BRG-004', 'Surya', 10, 10, 10),
-(6, 'BRG-005', 'Surya', 10, 10, 10);
+(4, 'BRG-004', 'Surya', 10, 10, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemasukan`
+--
+
+CREATE TABLE `pemasukan` (
+  `id` int(11) NOT NULL,
+  `kode_pemasukan` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemasukan`
+--
+
+INSERT INTO `pemasukan` (`id`, `kode_pemasukan`, `tanggal`) VALUES
+(1, 'MSK-00001', '2020-02-23'),
+(3, 'MSK-00002', '2020-03-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemasukan_detail`
+--
+
+CREATE TABLE `pemasukan_detail` (
+  `id` int(11) NOT NULL,
+  `id_pemasukan` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `small` int(11) NOT NULL,
+  `medium` int(11) NOT NULL,
+  `large` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemasukan_detail`
+--
+
+INSERT INTO `pemasukan_detail` (`id`, `id_pemasukan`, `id_barang`, `small`, `medium`, `large`) VALUES
+(9, 1, 1, 7, 8, 4),
+(10, 1, 4, 8, 6, 10),
+(11, 1, 3, 3, 3, 3),
+(12, 3, 2, 2, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id` int(11) NOT NULL,
+  `kode_pengeluaran` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id`, `kode_pengeluaran`, `tanggal`) VALUES
+(2, 'KLR-00001', '2020-02-27'),
+(4, 'KLR-00002', '2020-03-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengeluaran_detail`
+--
+
+CREATE TABLE `pengeluaran_detail` (
+  `id` int(11) NOT NULL,
+  `id_pengeluaran` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `small` int(11) NOT NULL,
+  `medium` int(11) NOT NULL,
+  `large` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengeluaran_detail`
+--
+
+INSERT INTO `pengeluaran_detail` (`id`, `id_pengeluaran`, `id_barang`, `small`, `medium`, `large`) VALUES
+(10, 2, 1, 8, 8, 9),
+(11, 2, 3, 7, 9, 7),
+(12, 2, 2, 2, 2, 2),
+(13, 4, 4, 4, 2, 4);
 
 --
 -- Indexes for dumped tables
@@ -84,6 +173,30 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
+-- Indexes for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pemasukan_detail`
+--
+ALTER TABLE `pemasukan_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengeluaran_detail`
+--
+ALTER TABLE `pengeluaran_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -98,6 +211,30 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pemasukan_detail`
+--
+ALTER TABLE `pemasukan_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pengeluaran_detail`
+--
+ALTER TABLE `pengeluaran_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
